82c8fef6b2bca3161339758d770b3b7c21c2c347
zeVACa <logunik@bk.ru>
2021-01-26 22:14:00 +0300

    basic layout

--- users-list-app/src/components/App.jsx
+++ users-list-app/src/components/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 
-import '../index.css';
+import '../index.scss';
 
-import InputSearch from './InputSearch';
-import CardList from './CardList';
-
-function App() {
-  console.log('рендер App');
+const App = () => {
+  fetch(`https://5c3755177820ff0014d92711.mockapi.io/users?page=1&limit=10`).then((res) => {
+    res.json().then((data) => {
+      console.log(data);
+      return data;
+    });
+  });
 
   const [inputValue, setInputValue] = React.useState('');
 
   return (
-    <div>
-      <div className="container">
-        <div className="col-sm-5">
-          <h1>{inputValue}</h1>
-          <InputSearch value={inputValue} setInputValue={setInputValue} />
-        </div>
-        <CardList inputValue={inputValue} />
+    <div className="app">
+      <ul className="users">
+        <li className="users__item">
+          <div className="users__item-name">Leanne Graham</div>
+          <div className="users__item-email">Sincere@april.biz</div>
+        </li>
+      </ul>
+      <div className="buttons">
+        <button>{'<-'}</button>
+        <button className="active">{'->'}</button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
